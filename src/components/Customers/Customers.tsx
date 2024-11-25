e1a0f3928ae1474cdef6aa231abc70811ed79b6b
ROFL1ST <danendrapr55@gmail.com>
2024-11-25 12:29:27 +0700

    feat: customer list

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -1,14 +1,217 @@
 "use client";
 import React from "react";
-
-
+import type { Customers } from "./components/data/interface";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Edit, HambergerMenu, Trash } from "iconsax-react";
+import Lottie from "react-lottie";
+import empty from "@/json/empty.json";
+import Add from "./components/Add";
+import { decryptData } from "./components/Decryption";
+import {
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+} from "@material-tailwind/react";
 const Customers: React.FC = () => {
+  const [customers, setCustomers] = React.useState<Customers[]>([]);
+  const [status, setStatus] = React.useState({ load: false, error: false });
+  const token = Cookies.get("token");
+
+  const getCustomers = async (key: string) => {
+    setStatus({ load: true, error: false });
+    try {
+      await axios
+        .get(
+          `${process.env.NEXT_PUBLIC_DEV_API}/customer${key !== "" ? `?keyword=${key}` : ``}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        )
+        .then((response) => {
+          console.log(response.data);
+          setCustomers(response.data.data);
+          setStatus({ load: false, error: false });
+        });
+    } catch (error) {
+      console.log(error);
+      setStatus({ load: false, error: true });
+    }
+  };
+
+  React.useEffect(() => {
+    getCustomers("");
+  }, []);
   return (
     <>
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-      <h1>Customers</h1>
+      <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <div className="flex items-center justify-between pb-4">
+          <h2 className="text-lg font-semibold text-black dark:text-white">
+            Agents
+          </h2>
+          <div className="flex gap-x-3">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="rounded-md border bg-white px-4 py-2 text-sm text-black dark:border-strokedark dark:bg-boxdark dark:text-white"
+              onChange={(e) => {
+                if (e.target.value.length > 2) {
+                  getCustomers(e.target.value);
+                } else {
+                  getCustomers("");
+                }
+              }}
+            />
+            <Add getCustomers={getCustomers} />
+          </div>
+        </div>
+        <div className="max-w-full overflow-x-auto">
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="bg-gray-2 text-left dark:bg-meta-4">
+                <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
+                  Referal Code
+                </th>
+                <th className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white">
+                  Name
+                </th>
+                <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                  Phone No.
+                </th>
+                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                  Email
+                </th>
+                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                  Date
+                </th>
+                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                  Agent Name
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {customers.map((data, key) => (
+                <CustomerList
+                  key={key}
+                  customers={data}
+                  getCustomers={getCustomers}
+                />
+              ))}
+            </tbody>
+          </table>
+          {customers.length === 0 && !status.load && (
+            <div className="flex justify-center">
+              <div className="w-1/4">
+                <Lottie
+                  options={{
+                    animationData: empty,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
+      {/* Edit */}
+
+      {/* Edit */}
     </>
+  );
+};
+
+const CustomerList: React.FC<{
+  customers: Customers;
+  getCustomers: (key: string) => void;
+}> = ({ customers, getCustomers }) => {
+  const [editOpen, setEditOpen] = React.useState<boolean>(false);
+  const [deleteOpen, setDeleteOpen] = React.useState<boolean>(false);
+  const decryptedRefCode = decryptData(customers.ref_code);
+  // console.log(decryptedRefCode, "HAI", customers.fullname);
+
+  return (
+    <tr>
+      <td className="border-b border-[#eee] px-4 py-5 pl-9 dark:border-strokedark xl:pl-11">
+        <h5 className="font-medium text-black dark:text-white">
+          {decryptedRefCode}
+        </h5>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        <p className="text-black dark:text-white">
+          {customers.first_name ?? customers.fullname ?? "Unknown"}
+        </p>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        <p className="text-black dark:text-white">{customers.phone}</p>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        <p className="text-black dark:text-white">{customers.email}</p>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        <p className="text-black dark:text-white">
+          {customers.ref_code_created_date
+            ? new Date(customers.ref_code_created_date).toLocaleDateString(
+                "en-US",
+                {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                },
+              )
+            : "Date not available"}
+        </p>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        <p className="text-black dark:text-white">
+          {customers.agent?.name ?? "Unknown"}
+        </p>
+      </td>
+      <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
+        {/* Dropdown Container */}
+        <Menu placement="bottom-end">
+          <MenuHandler>
+            <button className="">
+              <HambergerMenu size="24" variant="Bold" className="mr-2" />{" "}
+              {/* Burger Icon */}
+            </button>
+          </MenuHandler>
+
+          {/* Dropdown Menu */}
+          <MenuList
+            className=""
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            <MenuItem
+              onClick={() => setEditOpen(true)}
+              className="flex items-center text-sm text-gray-700 hover:bg-gray-100"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Edit size="18" variant="Bold" className="mr-2" />
+              Edit
+            </MenuItem>
+            <MenuItem
+              onClick={() => setDeleteOpen(true)}
+              className="flex items-center text-sm text-red-600 hover:bg-gray-100"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <Trash size="18" variant="Bold" className="mr-2" />
+              Delete
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </td>
+    </tr>
   );
 };
 
---
+++ src/components/Customers/components/Add.tsx
@@ -0,0 +1,162 @@
+"use client";
+import ModalPop from "@/components/ModalPop/ModalPop";
+import { Button, Dialog } from "@headlessui/react";
+import { Input, Typography } from "@material-tailwind/react";
+import React from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Cookies from "js-cookie";
+
+import { useForm, SubmitHandler, useWatch } from "react-hook-form";
+import axios from "axios";
+
+interface IFormInput {
+  name: string;
+}
+
+interface AddProps {
+  getCustomers: (key: string) => Promise<void>;
+}
+const schema = yup
+  .object({
+    name: yup.string().required("Name is required"),
+  })
+  .required();
+const Add: React.FC<AddProps> = ({ getCustomers }) => {
+  const token = Cookies.get("token");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+  const currentName = useWatch({ control, name: "name" }) || "";
+//   console.log(currentName);
+
+  const [status, setStatus] = React.useState({ load: false, error: false });
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    setStatus({ load: true, error: false });
+    try {
+      await axios
+        .post(`${process.env.NEXT_PUBLIC_DEV_API}/agent/create`, data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          reset();
+          setStatus({ load: false, error: false });
+          setIsOpen(false);
+          getCustomers("");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  return (
+    <>
+      <div className="">
+        <Button
+          onClick={() => {
+            setIsOpen(true);
+          }}
+          className="w-full cursor-pointer rounded-lg border border-primary bg-primary px-4 py-2 text-white transition hover:bg-opacity-90"
+        >
+          Add +
+        </Button>
+      </div>
+      <ModalPop
+        isOpen={isOpen}
+        closeModalAdd={() => {
+          setIsOpen(false);
+          reset();
+        }}
+      >
+        <Dialog.Title
+          as="h3"
+          className="mb-4 text-xl font-semibold text-gray-900 dark:text-white"
+        >
+          Add Agent
+        </Dialog.Title>
+
+        {/* Form Section */}
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          {/* Input Field */}
+          <div>
+            <label
+              htmlFor="agentName"
+              className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300"
+            >
+              Agent Name
+            </label>
+            <Input
+              crossOrigin={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+              {...register("name", { required: true })}
+              type="text"
+              className="w-full rounded-lg border border-stroke bg-transparent py-3 pl-3 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+            />
+            {errors.name && (
+              <Typography
+                className="mt-5 flex items-center gap-2 text-sm text-red-500"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                {errors.name.message}
+              </Typography>
+            )}
+          </div>
+
+          {/* Buttons Section */}
+          <div className="flex justify-end space-x-3">
+            {/* Cancel Button */}
+            <button
+              type="button"
+              onClick={() => {
+                setIsOpen(false);
+                reset();
+              }}
+              className="rounded-lg bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
+            >
+              Cancel
+            </button>
+
+            {/* Add Button */}
+            <button
+              disabled={status.load || !currentName.trim()} 
+              type="submit"
+              className={`rounded-lg px-4 py-2 text-sm font-medium text-white ${
+                status.load || !currentName.trim()
+                  ? "cursor-not-allowed bg-gray-400"
+                  : "bg-blue-600 hover:bg-blue-700"
+              } focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-blue-500 dark:hover:bg-blue-600`}
+            >
+              {status.load ? "Loading..." : "Add"}
+            </button>
+          </div>
+        </form>
+      </ModalPop>
+    </>
+  );
+};
+
+export default Add;
