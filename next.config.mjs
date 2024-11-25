30d189f6138cfbcfaf7491f6f9b4a5d1854d0943
ROFL1ST <danendrapr55@gmail.com>
2024-11-25 10:51:08 +0700

    fix: adding base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const basePath =
+  process.env.NEXT_PUBLIC_BASE_PATH && process.env.NEXT_PUBLIC_BASE_PATH !== "/"
+    ? process.env.NEXT_PUBLIC_BASE_PATH
+    : "";
+
+const nextConfig = {
+  basePath : basePath,
+  // Remove the redirects function entirely to avoid conflicts
+};
 
 export default nextConfig;
