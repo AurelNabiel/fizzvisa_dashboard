223a2fba94f30b5c16e34a825723f3a97289ccf9
ROFL1ST <danendrapr55@gmail.com>
2024-12-02 13:29:38 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,25 @@
 import { NextResponse, NextRequest } from "next/server";
 import * as cookie from "cookie";
+const basePath =
+  process.env.NEXT_PUBLIC_BASE_PATH && process.env.NEXT_PUBLIC_BASE_PATH !== "/"
+    ? process.env.NEXT_PUBLIC_BASE_PATH
+    : "";
 
 export function middleware(req: NextRequest) {
   const cookies = cookie.parse(req.headers.get("cookie") || ""); 
   const url = new URL(req.url);
-  
-  console.log("cookies", cookies);
+
+  // console.log("cookies", cookies);
 
 
   if (!cookies.token) {
-    if (!url.pathname.startsWith("/auth/signin")) {
-      return NextResponse.redirect(new URL("/auth/signin", req.url));
+    if (!url.pathname.startsWith(`${basePath}/auth/signin`)) {
+      return NextResponse.redirect(new URL("/admin/auth/signin", req.url));
     }
   } else {
  
-    if (url.pathname.startsWith("/auth/signin")) {
-      return NextResponse.redirect(new URL("/", req.url));
+    if (url.pathname.startsWith(`${basePath}/auth/signin`)) {
+      return NextResponse.redirect(new URL(`${basePath}/`, req.url));
     }
   }
 
@@ -24,5 +28,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/auth/signin", "/"], 
+  matcher: ["/auth/signin", "/customers", "/", "/agents", "/settings"], 
 };
