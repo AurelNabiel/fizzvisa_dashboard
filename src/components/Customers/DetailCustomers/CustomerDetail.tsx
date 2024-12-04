8d25267bd38dfc581ef414d1a2ae0db081383302
ROFL1ST <danendrapr55@gmail.com>
2024-12-04 13:37:41 +0700

    feat: customer detail(setup)

--- src/components/Customers/DetailCustomers/CustomerDetail.tsx
+++ src/components/Customers/DetailCustomers/CustomerDetail.tsx
@@ -2,11 +2,11 @@
 import React from "react";
 
 const CustomerDetail: React.FC = () => {
-    return (
-        <div>
-        <h1>Customer Detail</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Customer Detail</h1>
+    </div>
+  );
+};
 
 export default CustomerDetail;
