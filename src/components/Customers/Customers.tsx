601a5726745809b1364e4145276f606be7be277f
ROFL1ST <danendrapr55@gmail.com>
2024-12-08 13:20:27 +0700

    feat: filter agent for customer list

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -1,14 +1,29 @@
 "use client";
 import React from "react";
-import type { Customers } from "./components/data/interface";
+import type { Agent, Customers } from "./components/data/interface";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { Button } from "@headlessui/react";
+import {
+  Button,
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import clsx from "clsx";
 import Lottie from "react-lottie";
 import empty from "@/json/empty.json";
 import { useRouter } from "next/navigation";
 import { decryptData } from "./components/Decryption";
-import { Edit, HambergerMenu, More, Task, Trash } from "iconsax-react";
+import {
+  ArrowDown2,
+  Check,
+  Edit,
+  HambergerMenu,
+  More,
+  Task,
+  Trash,
+} from "iconsax-react";
 import {
   Menu,
   MenuHandler,
@@ -24,13 +39,17 @@ const Customers: React.FC = () => {
   const [totalPages, setTotalPages] = React.useState(1);
   const token = Cookies.get("token");
 
-  const getCustomers = async (key: string, page: number): Promise<void> => {
+  const getCustomers = async (
+    key: string,
+    page: number,
+    agent_id: number,
+  ): Promise<void> => {
     setStatus({ load: true, error: false });
     try {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_DEV_API}/customer?page=${page}&limit=10${
           key !== "" ? `&keyword=${key}` : ""
-        }`,
+        }${agent_id != 0 ? `&agent_ids=${agent_id}` : ""}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -45,10 +64,6 @@ const Customers: React.FC = () => {
       setStatus({ load: false, error: true });
     }
   };
-
-  React.useEffect(() => {
-    getCustomers("", page);
-  }, [page]);
 
   // select
   const [selectedCustomers, setSelectedCustomers] = React.useState<any[]>([]);
@@ -103,7 +118,7 @@ const Customers: React.FC = () => {
             message: "Link sent successfully",
           });
           setSelectedCustomers([]);
-          getCustomers("", page);
+          getCustomers("", page, selectedAgents?.id ?? 0);
           setTimeout(() => {
             setSubmitStatus({ load: false, error: false, message: "" });
           }, 3000);
@@ -120,25 +135,71 @@ const Customers: React.FC = () => {
       }, 3000);
     }
   };
+
+  // filter agent
+  const [agents, setAgents] = React.useState<Agent[]>([]);
+  const [agentStatus, setAgentStatus] = React.useState({
+    load: false,
+    error: false,
+  });
+  const [selectedAgents, setSelectedAgents] = React.useState<Agent | null>(null);
+  const getAgents = async (): Promise<void> => {
+    setAgentStatus({ load: true, error: false });
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_DEV_API}/agent`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      setAgents(response.data.data);
+      setAgentStatus({ load: false, error: false });
+    } catch (error) {
+      console.error(error);
+      setAgentStatus({ load: false, error: true });
+    }
+  };
+
+  React.useEffect(() => {
+    getAgents();
+  }, []);
+
+  React.useEffect(() => {
+    getCustomers("", page, selectedAgents?.id ?? 0);
+  }, [page, selectedAgents]);
+
+  React.useEffect(() => {
+    console.log(selectedAgents);
+  }, [selectedAgents]);
+
+  const handleFilter = (e: any) => {
+    console.log(e);
+    setSelectedAgents(e);
+    setPage(1);
+  };
+
+  // filter link sudah di send
   return (
     <>
       <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
         <div className="flex items-center justify-between pb-4">
-          <h2 className="text-lg font-semibold text-black dark:text-white">
-            Customers
-          </h2>
+          <h2 className="dark: text-lg font-semibold text-black">Customers</h2>
           <div className="flex gap-x-3">
             <input
               type="text"
               placeholder="Search..."
-              className="rounded-md border bg-white px-4 py-2 text-sm text-black dark:border-strokedark dark:bg-boxdark dark:text-white"
+              className="dark: rounded-md border bg-white px-4 py-2 text-sm text-black dark:border-strokedark dark:bg-boxdark"
               onChange={(e) => {
                 if (e.target.value.length > 2) {
-                  getCustomers(e.target.value, 1);
+                  getCustomers(e.target.value, 1, selectedAgents?.id ?? 0);
                   setPage(1); // Reset to first page on new search
+                  setSelectedAgents(null);
                 } else {
-                  getCustomers("", 1);
+                  getCustomers("", 1, selectedAgents?.id ?? 0);
                   setPage(1);
+                  setSelectedAgents(null);
                 }
               }}
             />
@@ -148,6 +209,64 @@ const Customers: React.FC = () => {
             >
               Add +
             </Button>
+            <Listbox value={selectedAgents} onChange={handleFilter}>
+              <div className="relative">
+                <ListboxButton
+                  className={
+                    "inline-flex items-center justify-between whitespace-nowrap rounded-md bg-primary px-5 py-3 font-semibold text-white"
+                  }
+                >
+                  {selectedAgents ? selectedAgents.name : "Agents"}
+                  <ArrowDown2
+                    className="group pointer-events-none ml-2 size-4 fill-white/60"
+                    aria-hidden="true"
+                  />
+                </ListboxButton>
+                <ListboxOptions
+                  anchor="bottom"
+                  transition
+                  className={
+                    "mt-1 max-h-60 overflow-auto rounded-md bg-primary py-1 text-base text-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                  }
+                >
+                  <ListboxOption
+                    value={""}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-6 pr-4 ${
+                        active ? "bg-blue-100 text-blue-900" : "text-white"
+                      }`
+                    }
+                  >
+                    <div
+                      className={`block cursor-pointer ${
+                        selectedAgents ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      Agents
+                    </div>
+                  </ListboxOption>
+                  {agents.map((person) => (
+                    <ListboxOption
+                      key={person.name}
+                      value={person}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-6 pr-4 ${
+                          active ? "bg-blue-100 text-blue-900" : "text-white"
+                        }`
+                      }
+                    >
+                      <div
+                        className={`block cursor-pointer ${
+                          selectedAgents ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </div>
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </div>
+            </Listbox>
           </div>
         </div>
 
@@ -166,7 +285,7 @@ const Customers: React.FC = () => {
           <table className="w-full table-auto">
             <thead>
               <tr className="bg-gray-2 text-left dark:bg-meta-4">
-                <th className="min-w-[40px] px-4 py-4 pl-9 font-medium text-black dark:text-white xl:pl-11">
+                <th className="dark: min-w-[40px] px-4 py-4 pl-9 font-medium text-black xl:pl-11">
                   <input
                     type="checkbox"
                     className="rounded-md border-gray-300 text-primary focus:border-primary"
@@ -174,25 +293,23 @@ const Customers: React.FC = () => {
                     onChange={handleSelectAll}
                   />
                 </th>
-                <th className="min-w-[220px] px-4 py-4 font-medium text-black dark:text-white xl:pl-11">
+                <th className="dark: min-w-[220px] px-4 py-4 font-medium text-black xl:pl-11">
                   Referal Code
                 </th>
-                <th className="min-w-[150px] px-4 py-4 font-medium text-black dark:text-white">
+                <th className="dark: min-w-[150px] px-4 py-4 font-medium text-black">
                   Name
                 </th>
-                <th className="min-w-[120px] px-4 py-4 font-medium text-black dark:text-white">
+                <th className="dark: min-w-[120px] px-4 py-4 font-medium text-black">
                   Phone No.
                 </th>
-                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                <th className="dark: px-4 py-4 font-medium text-black">
                   Email
                 </th>
-                <th className="px-4 py-4 font-medium text-black dark:text-white">
-                  Date
-                </th>
-                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                <th className="dark: px-4 py-4 font-medium text-black">Date</th>
+                <th className="dark: px-4 py-4 font-medium text-black">
                   Agent Name
                 </th>
-                <th className="px-4 py-4 font-medium text-black dark:text-white">
+                <th className="dark: px-4 py-4 font-medium text-black">
                   Actions
                 </th>
               </tr>
@@ -208,6 +325,7 @@ const Customers: React.FC = () => {
                       customers={data}
                       getCustomers={getCustomers}
                       currentPage={page}
+                      setSelected={setSelectedAgents}
                     />
                   ))
                 : [...Array(5)].map((_, key) => <CustomerLoader key={key} />)}
@@ -228,7 +346,9 @@ const Customers: React.FC = () => {
             </div>
           )}
         </div>
-        <div className={`${customers.length != 0 ? "flex" : "hidden"} items-center justify-between pb-3 pt-6`}>
+        <div
+          className={`${customers.length != 0 || customers.length > 10 ? "flex" : "hidden"} items-center justify-between pb-3 pt-6`}
+        >
           <nav aria-label="Pagination" className="flex items-center space-x-2">
             <button
               className={`rounded-lg border px-4 py-2 ${
@@ -305,7 +425,7 @@ const Customers: React.FC = () => {
           </nav>
           <button
             onClick={handleSubmitSelected}
-            className={` rounded-lg border  px-4 py-2 text-white hover:bg-opacity-90 ${selectedCustomers.length === 0 || submitStatus.load ? "cursor-not-allowed bg-gray-300" : "bg-primary"}`}
+            className={` rounded-lg border  px-4 py-2  hover:bg-opacity-90 ${selectedCustomers.length === 0 || submitStatus.load ? "cursor-not-allowed bg-gray-300" : "bg-primary"}`}
             disabled={selectedCustomers.length === 0 || submitStatus.load}
           >
             {submitStatus.load ? "Sending..." : "Send Link"}
@@ -319,10 +439,11 @@ const Customers: React.FC = () => {
 const CustomerList: React.FC<{
   currentPage?: number;
   customers: Customers;
-  getCustomers: (key: string, page: number) => Promise<void>;
+  getCustomers: (key: string, page: number, agent_id: number) => Promise<void>;
   selectedCustomers: any[];
   setSelectedCustomers: (value: any[]) => void;
   handleCheckboxChange: (customer: any) => void;
+  setSelected: (value: Agent | null) => void;
 }> = ({
   customers,
   getCustomers,
@@ -330,6 +451,7 @@ const CustomerList: React.FC<{
   selectedCustomers,
   setSelectedCustomers,
   handleCheckboxChange,
+  setSelected,
 }) => {
   const [assignOpen, setAssingOpen] = React.useState<boolean>(false);
   const [deleteOpen, setDeleteOpen] = React.useState<boolean>(false);
@@ -351,23 +473,21 @@ const CustomerList: React.FC<{
           />
         </td>
         <td className="border-b border-[#eee] px-4 py-5 pl-9 dark:border-strokedark xl:pl-11">
-          <h5 className="font-medium text-black dark:text-white">
-            {decryptedRefCode}
-          </h5>
+          <h5 className="dark: font-medium text-black">{decryptedRefCode}</h5>
         </td>
         <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-          <p className="text-black dark:text-white">
+          <p className="dark: text-black">
             {customers.first_name ?? customers.fullname ?? "Unknown"}
           </p>
         </td>
         <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-          <p className="text-black dark:text-white">{customers.phone}</p>
+          <p className="dark: text-black">{customers.phone}</p>
         </td>
         <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-          <p className="text-black dark:text-white">{customers.email}</p>
+          <p className="dark: text-black">{customers.email}</p>
         </td>
         <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-          <p className="text-black dark:text-white">
+          <p className="dark: text-black">
             {customers.ref_code_created_date
               ? new Date(customers.ref_code_created_date).toLocaleDateString(
                   "en-US",
@@ -381,7 +501,7 @@ const CustomerList: React.FC<{
           </p>
         </td>
         <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-          <p className="text-black dark:text-white">
+          <p className="dark: text-black">
             {customers.agent?.name ?? "Unknown"}
           </p>
         </td>
@@ -436,6 +556,7 @@ const CustomerList: React.FC<{
         getCustomers={getCustomers}
         deleteOpen={deleteOpen}
         setDeleteOpen={setDeleteOpen}
+        setSelected={setSelected}
       />
     </>
   );
--- src/components/Customers/components/Delete.tsx
+++ src/components/Customers/components/Delete.tsx
@@ -4,14 +4,16 @@ import React from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { Dialog } from "@headlessui/react";
+import { Agent } from "./data/interface";
 
 interface DeleteProps {
   id: number;
   fullname: string;
-  getCustomers: (key: string, page: number) => Promise<void>;
+  getCustomers: (key: string, page: number, agent_id: number) => Promise<void>;
   deleteOpen: boolean;
   setDeleteOpen: (value: boolean) => void;
   currentPage: number;
+  setSelected: (value: Agent | null) => void;
 }
 
 const DeleteCust: React.FC<DeleteProps> = ({
@@ -20,7 +22,8 @@ const DeleteCust: React.FC<DeleteProps> = ({
   getCustomers,
   deleteOpen,
   setDeleteOpen,
-  currentPage
+  currentPage,
+  setSelected
 }) => {
   const token = Cookies.get("token");
   const [status, setStatus] = React.useState({ load: false, error: false });
@@ -42,7 +45,8 @@ const DeleteCust: React.FC<DeleteProps> = ({
           console.log(response.data);
           setStatus({ load: false, error: false });
           setDeleteOpen(false);
-          getCustomers("", currentPage);
+          setSelected(null);
+          getCustomers("", currentPage, 0);
         });
     } catch (error) {
       console.log(error);
