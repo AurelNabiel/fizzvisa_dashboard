2e7280293bd45fc2ebdcb72ec8eb51a8c17c2f21
ROFL1ST <danendrapr55@gmail.com>
2025-04-21 13:15:49 +0700

    fix: user type default value

--- src/components/Users/components/Edit.tsx
+++ src/components/Users/components/Edit.tsx
@@ -174,7 +174,7 @@ const EditUsers: React.FC<EditProps> = ({
             </label>
             <div className="relative">
               <select
-                {...register("user_type", { required: true, value: role })}
+                {...register("user_type", { required: true, value: user_type })}
                 className="ease w-full cursor-pointer appearance-none rounded border border-slate-200 bg-transparent py-2 pl-3 pr-8 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-400 focus:border-slate-400 focus:shadow-md focus:outline-none"
               >
                 <option value="admin">Admin</option>
@@ -222,7 +222,7 @@ const EditUsers: React.FC<EditProps> = ({
           {currentUserType === "agent" || user_type === "agent" ? (
             <div>
               <label
-                htmlFor="role"
+                htmlFor="agent"
                 className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300"
               >
                 Agent
